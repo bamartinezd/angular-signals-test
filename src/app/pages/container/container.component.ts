d5ff5df07cb83b951cf1e90f854bdd4345909798
bamartinezd <alejandromardelg@gmail.com>
2025-01-19 19:59:40 -0500

    (angular-test-signals): test get input field & jspdf generation stuff

--- src/app/pages/container/container.component.ts
+++ src/app/pages/container/container.component.ts
@@ -3,18 +3,22 @@ import { MaterialTableComponent } from '../../components/material-table/material
 import { ManageUserStore } from '../manage-users/add-user-dialog/add-user-dialog.store';
 import {User} from '../manage-users/add-user-dialog/add-user-dialog.models';
 import {patchState} from '@ngrx/signals';
+import {jsPDF} from 'jspdf';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-container',
-  imports: [MaterialTableComponent],
+  imports: [MaterialTableComponent, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss'
 })
 
 export class ContainerComponent implements OnInit {
-
   readonly #store = inject(ManageUserStore);
   protected readonly users = computed(()=>this.#store.users())
+  inputValue?: string;
 
   ngOnInit(): void {
     this.#store.users;
@@ -31,5 +35,17 @@ export class ContainerComponent implements OnInit {
       passportNumber: "AT5353",
       dateOfBirth: new Date()
     });
+  }
+
+  generatePDF(favoriteFood: string) {
+    const pdf = new jsPDF();
+    pdf.text('Favorite food: ' + favoriteFood, 10, 10);
+    pdf.save('HelloWorld.pdf');
+  }
+
+  getInputValue() {
+    const pdf = new jsPDF();
+    pdf.text('Favorite food: ' + this.inputValue, 10, 10);
+    pdf.save('HelloWorld.pdf');
   }
 }
